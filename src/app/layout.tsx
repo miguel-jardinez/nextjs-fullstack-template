1994e1a291a52424973bc2ecbe24973aafdd1ca7
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-07-23 19:02:25 -0600

    Enhance global styles and layout structure by updating CSS variables for improved theming, integrating tw-animate-css, and wrapping the layout in TRPCReactProvider for better state management.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import { TRPCReactProvider } from "@expenses/trcp/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
   );
 }
